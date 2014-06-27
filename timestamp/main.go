558c8ae966012107bc54a25d825fef7971fecf69
Will Norris <will@willnorris.com>
2014-06-27 08:43:53 -0700

    add help text

--- timestamp/main.go
+++ timestamp/main.go
@@ -32,7 +32,36 @@ var (
 	utc = flag.Bool("utc", false, "parse times without timezones as UTC")
 )
 
+const usageText = `timestamp is a tool for printing timestamps in various formats.
+
+Usage:
+  timestamp [-utc] [time]
+
+timestamp will print the specified time in the following formats:
+  - unix timestamp (number of seconds since January 1, 1970 UTC)
+  - rfc 3339 timestamp in the specified timezone (if not UTC)
+  - rfc 3339 timestamp in UTC
+  - ordinal date (year and day of the year)
+  - epoch days (number of days since January 1, 1970 UTC) as decimal and
+    sexigesimal (newbase60) formatted. This is only printed if date is after
+    1970-01-01, and is always calculated based on UTC time.
+
+time can be specified as a full rfc 3339 timestamp, or just the date component
+(YYYY-MM-DD).  If no time is specified, the current system time will be used.
+
+time values without an explicit timezone will be interpreted as the local
+system timezone unless the -utc flag is provided.
+
+Flags:
+`
+
+func usage() {
+	fmt.Fprintf(os.Stderr, usageText)
+	flag.PrintDefaults()
+}
+
 func main() {
+	flag.Usage = usage
 	flag.Parse()
 
 	loc := time.Local
